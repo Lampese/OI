5c434cf63eac4a21e8c14279de8b1bab3167e82c
东灯 <43312495+Lampese@users.noreply.github.com>
2022-11-04 09:44:48 +0800

    11.4 上午

--- DP/计数/P1806.cpp
+++ DP/计数/P1806.cpp
@@ -1,1 +1,12 @@
- 
+#include<bits/stdc++.h>
+#define MAXN 505
+using namespace std;
+long long n,f[MAXN];
+int main(){
+    cin>>n;
+    f[0]=1;
+    for(register int i=1;i<=500;++i)
+        for(register int j=n;j>=i;--j)
+            f[j]+=f[j-i];
+    cout<<f[n]-1<<endl;
+}
--- test.cpp
+++ test.cpp
@@ -1,43 +1,109 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-bool mm[10][10];
-pair<int,int>vis[10];
-int t,n,m;
-inline void init(){
-    for(register int i=1;i<=9;++i)
-        for(register int j=1;j<=9;++j)
-            mm[i][j]=false;
-}
-inline bool check(int x){
-    init();
-    mm[vis[x].first][vis[x].second]=true;
-    for(register int i=1;i<=m;++i){
-        if(i==x)continue;
-        for(register int j=1;j<=n;++j)mm[vis[i].first][j]=mm[j][vis[i].second]=true;
+inline int read()
+{
+    int x = 0, w = 1;
+    char ch = getchar();
+    while ((ch < '0' ||
+            ch > '9') &&
+           ch != '-')
+        ch = getchar();
+    if (ch == '-')
+    {
+        w = -1;
+        ch = getchar();
     }
-    for(register int i=1;i<=n;++i)
-        for(register int j=1;j<=n;++j)
-            if(mm[i][j]==false)
-                return true;
-    return false;
-}
-int main(){
-    cin>>t;
-    while(t--){
-        cin>>n>>m;
-        for(register int i=1;i<=m;++i){
-            int x,y;
-            cin>>x>>y;
-            vis[i]={x,y};
-        }
-        bool flag=false;
-        for(register int i=1;i<=m;++i){
-            if(check(i)){
-                cout<<"YES"<<endl;
-                flag=true;
-                break;
-            }
-        }
-        if(!flag)cout<<"NO"<<endl;
+    while (ch >= '0' && ch <= '9')
+    {
+        x = (x << 1) + (x << 3) + (ch ^ 48);
+        ch = getchar();
     }
+    return x * w;
+}
+const int N = 500005;
+int n, m, cnt[205][205][1005], sum[205][205][1005], q, a[N], C[N], d[N], len, cntt, root[N];
+long long summ[N];
+bool cmp(int x, int y)
+{
+    return y < x;
+}
+void Discretize()
+{
+    sort(C + 1, C + 1 + m, cmp);
+    len = unique(C + 1, C + 1 + m) - C - 1;
+    for (int i = 1; i <= m; i++)
+    {
+        d[i] = lower_bound(C + 1, C + 1 + len, a[i]) - C;
+        cout<<d[i]<<" ";
+    }
+    cout<<endl;
+}
+struct Node
+{
+    int l, r, siz;
+    long long sum;
+} tree[N << 5];
+int build(int l, int r)
+{
+    int now = ++cntt;
+    tree[now] = {0, 0, 0, 0};
+    if (l < r)
+    {
+        int mid = (l + r) >> 1;
+        tree[now].l = build(l, mid);
+        tree[now].r = build(mid + 1, r);
+    }
+    return now;
+}
+int modify(int now, int l, int r, int v)
+{
+    cout<<l<<" "<<r<<endl;
+    int to = ++cntt;
+    tree[to] = tree[now];
+    tree[to].siz = tree[now].siz + 1;
+    tree[to].sum = tree[now].sum + C[v];
+    if (l < r)
+    {
+        int mid = (l + r) >> 1;
+        if (v <= mid)
+            tree[to].l = modify(tree[now].l, l, mid, v);
+        else
+            tree[to].r = modify(tree[now].r, mid + 1, r, v);
+    }
+    return to;
+}
+long long query(int x, int y, int l, int r, int v)
+{
+    //cout<<x<<" "<<y<<" "<<l<<" "<<r<<" "<<v<<endl;
+    if (l == r)
+    {
+        return tree[y].sum - tree[x].sum;
+    }
+    if (!v)
+        return 0;
+    int mid = (l + r) >> 1;
+    int siz = tree[tree[y].l].siz - tree[tree[x].l].siz;
+    if (v <= siz)
+        return query(tree[x].l, tree[y].l, l, mid, v);
+    else
+        return tree[tree[y].l].sum - tree[tree[x].l].sum + query(tree[x].r, tree[y].r, mid + 1, r, v - siz);
+}
+int main()
+{
+    m = read();
+    for (int i = 1; i <= m; i++)
+    {
+        a[i] = read();
+        C[i]=a[i];
+        summ[i] = summ[i - 1] + a[i];
+    }
+    Discretize();
+    // root[0] = build(1, len);
+    // for (int i = 1; i <= m; i++){
+    //     root[i] = modify(root[i - 1], 1, len, d[i]);
+    //     cout<<endl;
+    // }
+    // int x,y,h;
+    // cin>>x>>y>>h;
+    // cout<<query(root[x-1],root[y],1,len,h)<<endl;
 }
